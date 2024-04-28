efe08c6cf348f0360173f6294bb820fa82ad22ac
jesus <Jesusfernandorios93@gmail.com>
2024-04-28 00:31:42 -0700

    updated public folder to contain all images needed. Created new Navbar and Footer components.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 import React from "react";
 import Image from "next/image";
-import CustomButton from "./CustomButton";
+import { CustomButton } from "./index";
 
 const Hero = () => {
   const handleScroll = () => {};
@@ -22,6 +22,12 @@ const Hero = () => {
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
         />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src={"/hero.png"} alt="hero" fill className="object-contain" />
+          <div className="hero__image-overlay" />
+        </div>
       </div>
     </div>
   );
--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,6 @@
 import Hero from "./Hero";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import CustomButton from "./CustomButton";
 
-export { Hero };
+export { Hero, NavBar, Footer, CustomButton };
