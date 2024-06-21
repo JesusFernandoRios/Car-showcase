92500e951b44aa68e8c6cbfbfc3a23f5ac9993b3
jesus-rios <jesus.rios@atventurepartners.tech>
2024-06-21 16:05:05 -0700

    created a carCard component that will contain all vehicle information. need to search for a new api that will contain car image and combine onto carCard

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
-import { Hero, SearchBar, CustomFilter } from "@/components/index";
+import { Hero, SearchBar, CustomFilter, CarCard } from "@/components/index";
 import { fetchCars } from "@/utils";
 
 export default async function Home() {
   const allCars = await fetchCars();
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
   console.log(allCars);
   return (
@@ -21,6 +23,23 @@ export default async function Home() {
             <CustomFilter title="year" />
           </div>
         </div>
+
+        {!isDataEmpty ? (
+          <section>
+            {" "}
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className={"home__error-container"}>
+            {" "}
+            <h2 className="text-black text-xl font-bold">No results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
--- utils/index.ts
+++ utils/index.ts
@@ -1,19 +1,36 @@
-
 export async function fetchCars() {
-    const headers = {
-        'x-rapidapi-key': 'c348128281msh72c3e128372a28ap105f4ajsn469cb700696d',
-        'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
-    }
+  const headers = {
+    "x-rapidapi-key": "c348128281msh72c3e128372a28ap105f4ajsn469cb700696d",
+    "x-rapidapi-host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
 
-    try{
-        const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
-            headers: headers
-        });
+  try {
+    const response = await fetch(
+      "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
+      {
+        headers: headers,
+      }
+    );
 
-        const result = await response.json()
+    const result = await response.json();
 
-        return result
-    } catch(error) {
-        console.log(error)
-    }
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+    const basePricePerDay = 50; // Base rental price per day in dollars
+    const mileageFactor = 0.1; // Additional rate per mile driven
+    const ageFactor = 0.05; // Additional rate per year of vehicle age
+  
+    // Calculate additional rate based on mileage and age
+    const mileageRate = city_mpg * mileageFactor;
+    const ageRate = (new Date().getFullYear() - year) * ageFactor;
+  
+    // Calculate total rental rate per day
+    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+  
+    return rentalRatePerDay.toFixed(0);
+  };
